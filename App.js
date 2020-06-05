2d231190f3f898c0922ffb4f4b7446d61ba268bf
SaurabhPandey16 <saurabh.pandey.iips@gmail.com>
2020-06-05 09:31:04 +0530

    fusionchart added

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-//import react in our code. 
+import React, {Component} from 'react';
+//import react in our code.
 
 //Import react-navigation
-import { createAppContainer } from 'react-navigation'; /*saurabh*/
-import { createStackNavigator} from 'react-navigation-stack';
-import { WebView } from "react-native-webview";
+import {createAppContainer} from 'react-navigation'; /*saurabh*/
+import {createStackNavigator} from 'react-navigation-stack';
+import {WebView} from 'react-native-webview';
 import FirstPage from './FirstPage';
 import SecondPage from './SecondPage';
-//import all the screens we are going to switch 
-const App = createStackNavigator({
-  //Constant which holds all the screens like index of any book 
-    FirstPage: { screen: FirstPage }, 
+//import all the screens we are going to switch
+const App = createStackNavigator(
+  {
+    //Constant which holds all the screens like index of any book
+    FirstPage: {screen: FirstPage},
     //First entry by default be our first screen if we do not define initialRouteName
-    SecondPage: { screen: SecondPage }, 
+    SecondPage: {screen: SecondPage},
   },
   {
-    initialRouteName: 'FirstPage',
-  }
+    initialRouteName: 'SecondPage',
+  },
 );
 export default createAppContainer(App);
--- SecondPage.js
+++ SecondPage.js
@@ -1,27 +1,107 @@
 import React, {Component} from 'react';
-//import react in our code.
-import {StyleSheet, View, Text} from 'react-native';
-import {WebView} from 'react-native-webview';
-//import all the components we are going to use.
-
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import FusionCharts from 'react-native-fusioncharts';
 export default class SecondPage extends Component {
-  static navigationOptions = {
-    title: 'Second Page',
-    //Sets Header text of Status Bar
-  };
+  constructor(props) {
+    super(props);
+    // STEP 2- Define the categories representing the labels on the X-axis
+    const categories = [
+      {
+        category: [
+          {label: 'A(P1)'},
+          {label: 'A(P2)'},
+          {label: 'B(P1)'},
+          {label: 'B(P2)'},
+        ],
+      },
+    ];
+    //Construct the dataset comprising multiple series
+    const dataset = [
+      {
+        seriesname: 'Curr Vol',
+        data: [{value: '10'}, {value: '20'}, {value: '30'}, {value: '40'}],
+      },
+      {
+        seriesname: 'Prev Vol',
+        data: [{value: '40'}, {value: '30'}, {value: '20'}, {value: '10'}],
+      },
+      {
+        seriesname: 'Vol Chg',
+        data: [
+          {value: '-0.75'},
+          {value: '-0.33333333'},
+          {value: '0.5'},
+          {value: '3'},
+        ],
+      },
+    ];
+    //STEP 3 - Chart Configurations
+    const chartConfig = {
+      type: 'mscolumn2d',
+      width: '100%',
+      height: '400',
+      dataFormat: 'json',
+      dataSource: {
+        chart: {
+          theme: 'fusion',
+          caption: 'Customer Product Chart',
+          xAxisname: 'Customer(Product)',
+          yAxisName: 'Volume',
+          plotFillAlpha: '80',
+          divLineIsDashed: '1',
+          divLineDashLen: '1',
+          divLineGapLen: '1',
+        },
+        categories: categories,
+        dataset: dataset,
+      },
+    };
+    this.state = chartConfig;
+    this.libraryPath = Platform.select({
+      // Specify fusioncharts.html file location
+      android: {
+        uri: 'file:///android_asset/fusioncharts.html',
+      },
+      ios: require('./assets/fusioncharts.html'),
+    });
+  }
   render() {
-    const {navigate} = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>FusionChart</Text>
 
-    const {webUrl} = this.props.navigation.state.params;
-    return <WebView source={{uri: webUrl}} style={{marginTop: 20}} />;
+        <View style={styles.chartContainer}>
+          <FusionCharts
+            type={this.state.type}
+            width={this.state.width}
+            height={this.state.height}
+            dataFormat={this.state.dataFormat}
+            dataSource={this.state.dataSource}
+            libraryPath={this.libraryPath} // set the libraryPath property
+          />
+        </View>
+      </View>
+    );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    margin: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    padding: 10,
+  },
+
+  header: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+
+  chartContainer: {
+    height: 400,
+    borderColor: '#000',
+    borderWidth: 1,
   },
 });
